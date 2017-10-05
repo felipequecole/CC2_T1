b3eadd0914533b7ad4715ef93601846c69350544
italo <it-jorge@hotmail.com>
2017-10-05 15:57:48 -0300

    Gramatica alterada para incluir uma lista de mais_var : for vitas

--- src/t1/AnalisadorSemantico.java
+++ src/t1/AnalisadorSemantico.java
@@ -85,6 +85,7 @@ public class AnalisadorSemantico extends LABaseVisitor{
     String tipo = (String) visitTipo(ctx.tipo());
     TabelaDeSimbolos atual = escopos.topo();
 //   EntradaTabelaDeSimbolos etds = new EntradaTabelaDeSimbolos();
+    System.out.println(ctx.lista_mais_var.size());
     String simbolo = ctx.IDENT().getText();
     if(!atual.existeSimbolo(simbolo)){
       atual.adicionarSimbolo(simbolo, tipo);
